0f4a66f5e19225da3968f0256d20acd3b88fb5c9
RajeevKrSah <rajeevjnvkne@gmail.com>
2024-10-08 21:24:15 +0530

    Signup Page is working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
-
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Home from './pages/Home';
 
 function App() {
+
   return (
-    <div>Hello from frontend</div>
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<Navigate to="/login" />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/home' element={<Home />}/>
+      </Routes>
+    </div>
   );
 }
 
